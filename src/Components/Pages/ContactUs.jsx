7f6c0cbc72ee0176dd66f317d229d5ef3c923321
Harshvardhan Swami <HarshvardhanS@Cybertech.Com>
2020-10-14 23:27:02 +0530

    latest_14102020

--- src/Components/Pages/ContactUs.jsx
+++ src/Components/Pages/ContactUs.jsx
@@ -28,22 +28,13 @@ function Contactus() {
                 <div className="col-8">
                 
                         <div className="mb-3">
-                        <label for="exampleFormControlInput1" className="form-label text_box_label">Name</label>
-                        <input type="text" className="form-control text_box" id="exampleFormControlInput1" placeholder="name@example.com" />
-                        </div>
-                       <div className="mb-3">
-                        <label for="exampleFormControlInput1" className="form-label text_box_label">Email address</label>
-                        <input type="email" className="form-control text_box" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                         </div>
                         <div className="mb-3">
-                        <label for="exampleFormControlInput1" className="form-label text_box_label">Phone Number</label>
-                        <input type="tel" className="form-control text_box" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
-                        <div className="mb-3">
-                        <label for="exampleFormControlTextarea1" className="form-label text_area_label">Message</label>
-                        <textarea className="form-control text_box" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
-                    
                 </div>
                 
                 </div>
