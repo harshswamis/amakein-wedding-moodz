331a82921de514438de9f7a6d8cf56481b4a9f3b
Harshvardhan Swami <HarshvardhanS@Cybertech.Com>
2020-10-14 14:15:30 +0530

    Updates

---
+++ precache-manifest.785a9363b139f9aa1719f7c46fe2e9c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753a3d27a0546f5aea5ca0958d66f5b2",
+    "url": "/amakein-wedding-moodz/index.html"
+  },
+  {
+    "revision": "06a984559c5a66a06f19",
+    "url": "/amakein-wedding-moodz/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "02295c66af17e53eb23c",
+    "url": "/amakein-wedding-moodz/static/css/main.8985c9c1.chunk.css"
+  },
+  {
+    "revision": "06a984559c5a66a06f19",
+    "url": "/amakein-wedding-moodz/static/js/2.90550b0d.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/amakein-wedding-moodz/static/js/2.90550b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02295c66af17e53eb23c",
+    "url": "/amakein-wedding-moodz/static/js/main.da51655e.chunk.js"
+  },
+  {
+    "revision": "a0d2c49babf3048e78fd",
+    "url": "/amakein-wedding-moodz/static/js/runtime-main.0e0d624c.js"
+  },
+  {
+    "revision": "ffb198be9700b90a330489409f4498bb",
+    "url": "/amakein-wedding-moodz/static/media/img-1.ffb198be.jpg"
+  },
+  {
+    "revision": "cbad43abcab79c4ee3468a0a39974eda",
+    "url": "/amakein-wedding-moodz/static/media/img-home-background.cbad43ab.JPG"
+  }
+]);
